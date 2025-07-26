f92aca58693c5aaa4019fe43f3d81b75e7c48147
Pawan Hettiarachchi <159749345+RadioCat01@users.noreply.github.com>
2025-07-26 21:12:59 +0530

    Init

---
+++ app/src/test/java/com/writer/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.writer.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
